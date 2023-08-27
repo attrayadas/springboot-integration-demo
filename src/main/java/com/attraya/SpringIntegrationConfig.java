b135ce23c4b192928d2d0cdad7d8f90af5acd27d
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-27 12:50:03 +0530

    Create FileWritingMessageHandler to write files in a  directory

--- src/main/java/com/attraya/SpringIntegrationConfig.java
+++ src/main/java/com/attraya/SpringIntegrationConfig.java
@@ -3,8 +3,11 @@ package com.attraya;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.InboundChannelAdapter;
+import org.springframework.integration.annotation.Poller;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.file.FileReadingMessageSource;
+import org.springframework.integration.file.FileWritingMessageHandler;
 
 import java.io.File;
 
@@ -13,10 +16,19 @@ import java.io.File;
 public class SpringIntegrationConfig {
 
     @Bean
-    @InboundChannelAdapter(value = "fileInputChannel")
+    @InboundChannelAdapter(value = "fileInputChannel", poller = @Poller(fixedDelay = "1000") ) // read automatically in 1secs
     public FileReadingMessageSource fileReadingMessageSource(){
         FileReadingMessageSource reader = new FileReadingMessageSource();
         reader.setDirectory(new File("C:\\Users\\Attraya\\Desktop\\Sources"));
         return reader;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileInputChannel")
+    public FileWritingMessageHandler fileWritingMessageHandler(){
+        FileWritingMessageHandler writer = new FileWritingMessageHandler(new File("C:\\Users\\Attraya\\Desktop\\Destination"));
+        writer.setAutoCreateDirectory(true); // to create "Destination" folder automatically
+        writer.setExpectReply(false);
+        return writer;
     }
 }
