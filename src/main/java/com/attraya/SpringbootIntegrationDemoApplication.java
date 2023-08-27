945d7cf108185aed5ce31a79c70e68f2979cd207
Attraya Das <attrayaghoshdas@gmail.com>
2023-08-27 12:30:51 +0530

    Initial commit

---
+++ src/main/java/com/attraya/SpringbootIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.attraya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attraya/SpringbootIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attraya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
